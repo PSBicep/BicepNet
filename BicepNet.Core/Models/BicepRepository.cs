7ac1ed0bc8430cc741dbb8c8fc022f34cfc2aa3b
Emanuel Palm <ep__palm@hotmail.com>
2021-12-14 16:37:17 +0100

    ToString() for Repository models

--- BicepNet.Core/Models/BicepRepository.cs
+++ BicepNet.Core/Models/BicepRepository.cs
@@ -11,5 +11,10 @@ namespace BicepNet.Core.Models
         public string Name { get; set; }
         public string Endpoint { get; set; }
         public IList<BicepRepositoryModule> ModuleVersions { get; set; } = new List<BicepRepositoryModule>();
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
