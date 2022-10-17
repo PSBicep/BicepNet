64258edf2daed04a969addb8c628f9bd263d03fb
Simon Wåhlin <simon.wahlin@advania.com>
2022-10-17 14:40:28 +0200

    Update version to 0.11.1 (#55)

--- BicepNet.Core/Azure/ManagementGroupHelper.cs
+++ BicepNet.Core/Azure/ManagementGroupHelper.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Linq;
 
 namespace BicepNet.Core.Azure;
 
--- BicepNet.Core/BicepWrapper.Decompile.cs
+++ BicepNet.Core/BicepWrapper.Decompile.cs
@@ -1,16 +1,12 @@
 using Bicep.Core.FileSystem;
 using Bicep.Decompiler;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace BicepNet.Core;
 
 public partial class BicepWrapper
 {
-    public static IDictionary<string, string> Decompile(string templatePath, string? outputDir = null, string? outputFile = null) => 
-        joinableTaskFactory.Run(() => DecompileAsync(templatePath, outputDir, outputFile));
-
-    public static async Task<IDictionary<string, string>> DecompileAsync(string templatePath, string? outputDir = null, string? outputFile = null)
+    public static IDictionary<string, string> Decompile(string templatePath, string? outputDir = null, string? outputFile = null)
     {
         var inputPath = PathHelper.ResolvePath(templatePath);
         var inputUri = PathHelper.FilePathToFileUrl(inputPath);
@@ -19,13 +15,13 @@ public partial class BicepWrapper
         var outputPath = PathHelper.ResolveDefaultOutputPath(inputPath, outputDir, outputFile, DefaultOutputPath);
         var outputUri = PathHelper.FilePathToFileUrl(outputPath);
 
-        var template = new Dictionary<string,string>();
+        var template = new Dictionary<string, string>();
         var templateDecompiler = new TemplateDecompiler(featureProvider, namespaceProvider, fileResolver, moduleRegistryProvider, bicepAnalyzer);
         var (entrypointUri, filesToSave) = templateDecompiler.DecompileFileWithModules(inputUri, outputUri);
 
         foreach (var (fileUri, bicepOutput) in filesToSave)
         {
-            template.Add(fileUri.LocalPath,bicepOutput);
+            template.Add(fileUri.LocalPath, bicepOutput);
         }
 
         return template;
